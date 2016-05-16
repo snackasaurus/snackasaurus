f25dc28a5f358e9693ddaea12b102c8bda8b783a
Christopher Chen <chrisc94@cs.washington.edu>
2016-05-15 18:55:15 -0700

    updated web files

--- client/index.js
+++ client/index.js
@@ -1,26 +1,38 @@
 // Snackasaurus UI
 $( document ).ready(function() {
-    $( '#submit' ).click(function() {
-      var name = $( '#name' ).val();
+
+    $( '#place-order' ).click(function() {
+
+      //alert("hello");
+      var name = $( '#clientname' ).val();
       var loct = $( '#location option:selected' ).text();
-      var snacks = {snack1 : 0, snack2 : 0,
-                    snack3 : 0, snack4 : 0,
-                    snack5 : 0, snack6 : 0};
-      var snacks_str = ''
-      jQuery.each(obj, function(i, val) {
-        snacks_str += i + '-' + val + ',';
-      });
-      if (snacks_str[snacks_str.length - 1] == ',') {
-        snacks_str = snacks_str.substring(0, snacks_str.length - 1);
-      }
+      
+      var snickers = document.getElementById('snickers').checked
+      var snickersAmount = $( '#snicker_amount option:selected' ).text();
+      
+      var cheetos = document.getElementById('cheetos').checked
+      var cheetosAmount = $( '#cheetos_amount option:selected' ).text();
+
+      //var snacks = {snack1 : 0, snack2 : 0,
+       //             snack3 : 0, snack4 : 0,
+       //             snack5 : 0, snack6 : 0};
+      //var snacks_str = ''
+      //jQuery.each(obj, function(i, val) {
+      //  snacks_str += i + '-' + val + ',';
+      //});
+      //if (snacks_str[snacks_str.length - 1] == ',') {
+      //  snacks_str = snacks_str.substring(0, snacks_str.length - 1);
+      //}
       console.log(name);
       console.log(loct);
-      console.log(snacks_str);
-      $.get( 'attu.cs.washington.edu:50000', { name: name, location : loct,
-                                               sncaks: snacks_str
-      }, function(data) {
-        var code = data.code;
-        // do something to display the code
-      });
+      //console.log(snacks_str);
+      //$.get( 'localhost:59595', { name: name, location : loct });
+      //$.get( 'attu4.cs.washington.edu:59595', { name: name, location : loct });
+                                               //sncaks: snacks_str
+      var xhttp = new XMLHttpRequest();
+      xhttp.open("GET", "http://127.0.0.1:59595", true);
+      xhttp.setRequestHeader("Access-Control-Allow-Origin", "http://127.0.0.1:59595");
+      xhttp.send();
+
     });
 });
