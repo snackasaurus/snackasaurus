da1b28c72edb2e03e9cd677f8d4ce7a829e10796
Chris <chrisc94@cs.washington.edu>
2016-05-17 13:20:04 -0700

    Snacks now get passed in GET request

--- static/index.js
+++ static/index.js
@@ -5,12 +5,40 @@ $( document ).ready(function() {
       //alert("hello");
       var name = $( '#clientname' ).val();
       var loct = $( '#location option:selected' ).text();
-      
+
+      var snacks_str = "";
+
       var snickers = document.getElementById('snickers').checked
-      var snickersAmount = $( '#snicker_amount option:selected' ).text();
-      
+      if (snickers) {
+        snacks_str += "&snickers=" + $( '#snicker_amount option:selected' ).text();
+      }
+
       var cheetos = document.getElementById('cheetos').checked
-      var cheetosAmount = $( '#cheetos_amount option:selected' ).text();
+      if (cheetos) {
+        snacks_str += "&cheetos=" + $( '#cheetos_amount option:selected' ).text();
+      }
+
+      var mandms = document.getElementById('mandms').checked
+      if (mandms) {
+        snacks_str += "&mandms=" + $( '#mandms_amount option:selected' ).text();
+      }
+
+      var twix = document.getElementById('twix').checked
+      if (twix) {
+        snacks_str += "&twix=" + $( '#twix_amount option:selected' ).text();
+      }
+
+      var doritos = document.getElementById('doritos').checked
+      if (doritos) {
+        snacks_str += "&doritos=" + $( '#doritos_amount option:selected' ).text();
+      }
+
+      var skittles = document.getElementById('skittles').checked
+      if (skittles) {
+        snacks_str += "&skittles=" + $( '#skittles_amount option:selected' ).text();
+      }
+
+      alert(snacks_str)
 
       //var snacks = {snack1 : 0, snack2 : 0,
        //             snack3 : 0, snack4 : 0,
@@ -38,7 +66,7 @@ $( document ).ready(function() {
       var code = Math.floor(Math.random()*9000) + 1000;
 
       var xhttp = new XMLHttpRequest();
-      xhttp.open("GET", "http://unimate.cs.washington.edu:59595/order?name=" + name + "&location=" + loct + "&code=" + code, true);
+      xhttp.open("GET", "http://unimate.cs.washington.edu:59595/order?name=" + name + "&location=" + loct + "&code=" + code + snacks_str, true);
       xhttp.setRequestHeader("Access-Control-Allow-Origin", "http://unimate.cs.washington.edu:59595");
       xhttp.send();
       xhttp.onload = function(event)
