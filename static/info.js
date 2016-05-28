70120e194691a34a181d53ac8f2887540b484815
Chris <chrisc94@cs.washington.edu>
2016-05-28 13:52:05 -0700

    info

--- static/info.js
+++ static/info.js
@@ -1,11 +1,26 @@
 // Snackasaurus UI
 $( document ).ready(function() {
-   var ws = new WebSocket("http://localhost:48101");
-   ws.onopen = function() {
-      console.log("y");
-   }
-
-   ws.onmessage = function(e) {
-      console.log(e);
-   }
+  setTimeout(function () {
+    var json = [];
+    $('.list-group').empty();
+    var i = 0;
+    $.getJSON("localhost:59595/info", function(data){
+      $.each(data, function(obj) {
+          var name = obj['name'];
+          var location = obj['location'];
+          if (i == 0) {
+            $('.list-group').append('<a href="#" class="list-group-item active">' +
+            '<h4 class="list-group-item-heading">Now delivering to: '+ name +'</h4>' +
+            '<p class="list-group-item-text">Location: '+location+'</p>')
+            $('.list-group').append('</a>')
+          } else {
+            $('.list-group').append('<a href="#" class="list-group-item">' +
+            '<h4 class="list-group-item-heading">' + name +'</h4>' +
+            '<p class="list-group-item-text">Location: '+location+'</p>')
+            $('.list-group').append('</a>')
+          }
+          i++;
+      });
+    });
+  }, 60000);
 });
